e036c9468a5fefd897afba7aa156af205e1e4c2a
sylvainSUPINTERNET <sylvain.joly00@orange.fr>
2021-08-24 00:29:44 +0200

    [UPDAT]

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,14 +1,47 @@
-
 import type { NextPage } from 'next'
+import { useEffect , useRef} from 'react';
 import { Profile } from '../types/Profile'
 
+const getDevicesAndApplyStream = async (videoObject: any) => {
+    const devices = await window.navigator
+                            .mediaDevices
+                            .enumerateDevices();
+    
+
+    try { 
+        const stream = await window.navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        });
+    
+        videoObject.srcObject = stream;
+        videoObject.play();
+    } catch ( e ) {
+        console.log("Error : ", e);
+    }
+
+}
+
 const Profiles = ({ profiles } : {profiles: Profile[] } ) => {
+    let videoObject:any = useRef(null);
+
+    useEffect( () => {
+        getDevicesAndApplyStream(videoObject.current);
+    })
+
     return (
         <div>
+
+            <video ref={videoObject}>
+
+            </video>
             {
                 profiles.length > 0 && profiles.map( (profile:Profile, i:number) => {
                     return <div key={i}>
-                        {profile.name}
+                        <div className="card">
+                            {profile.name}
+                            <button className="btn btn-primary">Ok</button>
+                        </div>
                     </div>
                 })
             }
