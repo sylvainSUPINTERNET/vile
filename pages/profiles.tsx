e356a77e45792d02a04f625c9688273b9916108d
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-08-28 23:18:37 +0200

    [WIP] rtc

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -36,48 +36,96 @@ const peerInit = async () => {
 const peerInstance = async (hubConnection: any) => {
     const Peer:any = await peerInit();
     let p = new Peer(nanoid())
-    hubConnection.invoke("SendMessage", "Hello", p._id)
+
+    hubConnection.invoke("JoinLake", p._id);
+    hubConnection.invoke("FindSomeone", p._id);
+
+    return new Promise( resolve => resolve(p));
+
 }
 
 
+// https://stackoverflow.com/questions/58975883/prevent-rerender-for-every-setstate-using-react-usestate-hook
+
 const Profiles = ({ profiles } : {profiles: Profile[] } ) => {
     let videoObject:any = useRef(null);
-    // let [peer, setPeer]: any = useState(import('peerjs').then( async ({ default: Peer }) => {
-    //     let p = await new Peer();
-    //     return p;
-    // } ));
+    let [hubConnection, setHubConnection] = useState({});
 
-
-    //import('peerjs').then( data => console.log(data));
+    let peerConnectionRef = useRef();
 
     useEffect( () => {
+
         getDevicesAndApplyStream(videoObject.current);
 
-        
-        let connection = new signalR.HubConnectionBuilder()
-            .withUrl("https://localhost:5001/profilehub")
-            .build();
+        let connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:5001/profilehub").build();
+        // @ts-ignore
+        setHubConnection(connection);
 
-        connection.on("send", data => {
-            console.log(data);
+        connection.on('newPeerOfferSend', ( targetPeerId:string ) => {
+            console.log("NEW OFFER SEND WITH SUCCESS");
+            console.log("TARGET PEER ID ", targetPeerId);
+
+            //@ts-ignore
+            let pConn = peerConnectionRef.current.connect(targetPeerId);
+            pConn.on('data', (data:any) => {
+                console.log("LIB", data);
+            })
+
+
         });
 
+        connection.on('newPeerOffer', async ( offerPeerId:string ) => {
+            console.log("Received new offer");
+            console.log("NEW OFFER RECEIVED, DO U ACCEPT ?", offerPeerId);
 
-        connection.start()
-            .then(() => {
-                peerInstance(connection);
+            console.log("NEW OFFER", peerConnectionRef);
+            console.log(offerPeerId)
 
-            });
+            //@ts-ignore
+            let pConn = peerConnectionRef.current.connect(offerPeerId);
+            pConn.on('open', () => {
+                console.log("OPEN")
+                pConn.send("HELLO");
+            }) 
 
-    })
+
+            
+
+            //console.log(peerConnection);
+            // const peerConn = Peer.connect(offerPeerId)
+            // peerConn.on('open', () => {
+            //     peerConn.send('hi!');
+            //   });
+        })
+    }, []);
+
+
+    const onClickStart = (ev:any) => {
+        if ( hubConnection ) {
+            //@ts-ignore
+            hubConnection.start()
+                .then( async () => {
+                    let m = await peerInstance(hubConnection);
+
+                    //@ts-ignore
+                    peerConnectionRef.current = m;
+                });
+        }    
+    }
+
+    const onClickStop = (ev:any) => {
+    }
 
     return (
         <div>
 
+            <button onClick={ onClickStart }>Start</button>
+            <button onClick={ onClickStop }>Stop</button>
+
             <video ref={videoObject}>
 
             </video>
-            {
+            {/* {
                 profiles.length > 0 && profiles.map( (profile:Profile, i:number) => {
                     return <div key={i}>
                         <div className="card">
@@ -88,6 +136,7 @@ const Profiles = ({ profiles } : {profiles: Profile[] } ) => {
                 })
             }
             <p>Lets go : {profiles.length}</p>
+             */}
         </div>
     )
 }
