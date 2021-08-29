0785e9a13fbf7202e565e81424b8ad7a6ec0bf0f
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-08-29 20:29:59 +0200

    [UPDATE]

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -6,25 +6,26 @@ import { nanoid } from 'nanoid'
 
 //import Peer from 'peerjs';
 
-const getDevicesAndApplyStream = async (videoObject: any) => {
-    const devices = await window.navigator
-                            .mediaDevices
-                            .enumerateDevices();
+// const getDevicesAndApplyStream = async (videoObject: any) => {
+//     const devices = await window.navigator
+//                             .mediaDevices
+//                             .enumerateDevices();
     
 
-    try { 
-        const stream = await window.navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true
-        });
+//     try { 
+//         const stream = await window.navigator.mediaDevices.getUserMedia({
+//             video: true,
+//             audio: true
+//         });
     
-        videoObject.srcObject = stream;
-        videoObject.play();
-    } catch ( e ) {
-        console.log("Error : ", e);
-    }
+//         videoObject.srcObject = stream;
+//         videoObject.play();
+//         return new Promise( resolve => resolve(stream));
+//     } catch ( e ) {
+//         console.log("Error : ", e);
+//     }
 
-}
+// }
 
 const peerInit = async () => {
     const peerjs = await import('peerjs');
@@ -49,53 +50,89 @@ const peerInstance = async (hubConnection: any) => {
 
 const Profiles = ({ profiles } : {profiles: Profile[] } ) => {
     let videoObject:any = useRef(null);
+    let videoObjectOther:any = useRef(null);
+
     let [hubConnection, setHubConnection] = useState({});
 
     let peerConnectionRef = useRef();
 
     useEffect( () => {
+        
 
-        getDevicesAndApplyStream(videoObject.current);
+        //getDevicesAndApplyStream(videoObject.current);
 
         let connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:5001/profilehub").build();
         // @ts-ignore
         setHubConnection(connection);
 
-        connection.on('newPeerOfferSend', ( targetPeerId:string ) => {
-            console.log("NEW OFFER SEND WITH SUCCESS");
-            console.log("TARGET PEER ID ", targetPeerId);
 
+        // Receiver
+        connection.on('newPeerOfferSend', async ( targetPeerId:string ) => {
+
+            const stream = await window.navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: true
+            });
+
+            console.log(videoObject);
+
+            videoObject.current.srcObject = stream;
+            videoObject.current.play();
+
+            
             //@ts-ignore
-            let pConn = peerConnectionRef.current.connect(targetPeerId);
-            pConn.on('data', (data:any) => {
-                console.log("LIB", data);
+            peerConnectionRef.current.on('call' , (call:any) => {
+                call.answer(stream); // answer with stream
+                call.on('stream', (remoteStream:any) => {
+                    console.log("ANSWER CALL");
+                    console.log(remoteStream);
+                        
+                                            
+                    videoObjectOther.current.srcObject = remoteStream;
+                    videoObjectOther.current.play();
+                    /// Show stream in some <video> element.
+                })
             })
 
 
-        });
-
-        connection.on('newPeerOffer', async ( offerPeerId:string ) => {
-            console.log("Received new offer");
-            console.log("NEW OFFER RECEIVED, DO U ACCEPT ?", offerPeerId);
-
-            console.log("NEW OFFER", peerConnectionRef);
-            console.log(offerPeerId)
 
             //@ts-ignore
-            let pConn = peerConnectionRef.current.connect(offerPeerId);
-            pConn.on('open', () => {
-                console.log("OPEN")
-                pConn.send("HELLO");
-            }) 
+            // peerConnectionRef.current.on('connection', (conn:any) => {
+            //     conn.on('data', (data:any) => {
+            //         console.log(data);
+            //       });
+            // })
+        });
 
 
-            
+        // Initiator
+        connection.on('newPeerOffer', async ( offerPeerId:string ) => {
+            const stream = await window.navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: true
+            });
 
-            //console.log(peerConnection);
-            // const peerConn = Peer.connect(offerPeerId)
-            // peerConn.on('open', () => {
-            //     peerConn.send('hi!');
-            //   });
+                            
+            videoObject.current.srcObject = stream;
+            videoObject.current.play();
+
+            //@ts-ignore
+            const call = peerConnectionRef.current.call(offerPeerId, stream);
+            call.on('stream', (remoteStream: any) => {
+                console.log("LESSS GO STREAM23°")
+                console.log(remoteStream);
+           
+                videoObjectOther.current.srcObject = remoteStream;
+                videoObjectOther.current.play();
+                    // Show stream in some <video> element.
+            })
+
+            //@ts-ignore
+            //let pConn = peerConnectionRef.current.connect(offerPeerId);
+            // pConn.on('open', () => {
+            //     console.log("OPEN")
+            //     pConn.send("HELLO");
+            // });
         })
     }, []);
 
@@ -122,9 +159,25 @@ const Profiles = ({ profiles } : {profiles: Profile[] } ) => {
             <button onClick={ onClickStart }>Start</button>
             <button onClick={ onClickStop }>Stop</button>
 
-            <video ref={videoObject}>
+            <div style={{"background": "green", "display": "flex", "justifyContent": "center", "flexFlow": "column"}}>
 
-            </video>
+                <div style={{"display": "flex"}}>
+                    <div style={{"background": "red", "flex": "0 0 15%"}} >
+                            <video ref={videoObjectOther} style={{"maxWidth": "30em"}}>
+                            </video>
+                    </div>
+                </div>
+
+                <div style={{"display": "flex"}}>
+                    <div style={{"background": "blue", "flex": "0 0 15%"}} >
+                            <video ref={videoObject} style={{"maxWidth": "30em"}}>
+                            </video>
+                    </div>
+                </div>
+            </div>
+
+
+
             {/* {
                 profiles.length > 0 && profiles.map( (profile:Profile, i:number) => {
                     return <div key={i}>
